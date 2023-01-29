a2cbb8ed4776e7190344b45c7814bd7f167c34d4
Deepa <deepa@Deepas-MacBook-Pro.local>
2023-01-29 17:04:22 +0530

    Added Config form for locations

---
+++ src/Form/LocationSettingsForm.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Drupal\location\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Cache\Cache;
+
+/**
+ * Configure location settings for this site.
+ */
+class LocationSettingsForm extends ConfigFormBase {
+
+  /** 
+   * Config settings.
+   *
+   * @var string
+   */
+  const SETTINGS = 'location.settings';
+
+  /** 
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'location_admin_settings';
+  }
+
+  /** 
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      static::SETTINGS,
+    ];
+  }
+
+  /** 
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config(static::SETTINGS);
+
+    $form['city'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('City'),
+      '#default_value' => $config->get('city') ?: 'New York, NY',
+    ];  
+
+    $form['country'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Country'),
+      '#default_value' => $config->get('country') ?: 'USA',
+    ];
+
+    $timezone_options = [
+      'America/Chicago' => 'America/Chicago',
+      'America/New_York' => 'America/New_York',
+      'Asia/Tokyo' => 'Asia/Tokyo',
+      'Asia/Dubai' => 'Asia/Dubai',
+      'Asia/Kolkata' => 'Asia/Kolkata',
+      'Europe/Amsterdam' => 'Europe/Amsterdam',
+      'Europe/Oslo' => 'Europe/Oslo',
+      'Europe/London' => 'Europe/London',
+    ];
+
+    $form['timezone'] = [
+      '#type' => 'select',
+      '#options' => $timezone_options,
+      '#title' => 'Select Timezone',
+      '#required' => TRUE,
+      '#default_value' => $config->get('timezone') ?: 'America/New_York',
+    ];
+
+    $form['#cache']['tags'] = ['location_admin_settings'];
+    Cache::mergeTags($form['#cache']['tags'], $config->getCacheTags());
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /** 
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->config(static::SETTINGS)
+      ->set('city', $form_state->getValue('city'))
+      ->set('country', $form_state->getValue('country'))
+      ->set('timezone', $form_state->getValue('timezone'))
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
